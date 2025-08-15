c71b50aa91f75f9abe2bfa6e740e8c7e0916b2fb
iZezito <emersonsilva81240@gmail.com>
2025-08-15 11:27:16 -0300

    refact: login via google

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -8,8 +8,6 @@ import * as arctic from "arctic";
 import { UserService } from "../user/service";
 import { User } from "../../generated/prismabox/User";
 
-const pkceStore = new Map<string, { state: string; codeVerifier: string }>();
-
 export const authController = new Elysia({ prefix: "/auth" })
   .use(jwtService)
   .post(
@@ -41,55 +39,66 @@ export const authController = new Elysia({ prefix: "/auth" })
       body: authSchema,
     }
   )
-  .get("/oauth/google", async ({ redirect }) => {
-    console.log("Chama na poiva");
+  .decorate(
+    "pkceStore",
+    new Map<string, { state: string; codeVerifier: string }>()
+  )
+  .get("/oauth/google", async ({ redirect, pkceStore }) => {
     const state = arctic.generateState();
     const codeVerifier = arctic.generateCodeVerifier();
     const scopes = ["openid", "profile", "email"];
 
     const url = google.createAuthorizationURL(state, codeVerifier, scopes);
     url.searchParams.set("access_type", "offline");
-
     pkceStore.set(state, { state, codeVerifier });
+
     console.log(url.toString());
     return redirect(url.toString());
   })
-  .get("/oauth/google/callback", async ({ query, status, jwt }) => {
-    const code = query.code as string;
-    const state = query.state as string;
+  .get(
+    "/oauth/google/callback",
+    async ({ query, status, jwt, pkceStore, redirect }) => {
+      const code = query.code as string;
+      const state = query.state as string;
 
-    const sessionData = pkceStore.get(state);
-    if (!sessionData) {
-      return status(400, { error: "Invalid state" });
-    }
-
-    try {
-      const tokens = await google.validateAuthorizationCode(
-        code,
-        sessionData.codeVerifier
-      );
-
-      const accessToken = tokens.accessToken();
-      const idToken = tokens.idToken();
-      const claims = arctic.decodeIdToken(idToken) as GoogleIdTokenClaims;
-
-      const user = await UserService.save({
-        password: "ttttttttttttt",
-        oauth2Provider: "google",
-        email: claims.email,
-        name: claims.name,
-      });
-
-      const token = await jwt.sign({ userId: user.id });
-
-      return { token, user };
-    } catch (e) {
-      if (e instanceof arctic.OAuth2RequestError) {
-        return status(400, { error: "Invalid authorization code" });
+      const sessionData = pkceStore.get(state);
+      if (!sessionData) {
+        return status(400, { error: "Invalid state" });
       }
-      if (e instanceof arctic.ArcticFetchError) {
-        return status(500, { error: "Fetch error" });
+
+      try {
+        const tokens = await google.validateAuthorizationCode(
+          code,
+          sessionData.codeVerifier
+        );
+
+        const accessToken = tokens.accessToken();
+        const idToken = tokens.idToken();
+        const claims = arctic.decodeIdToken(idToken) as GoogleIdTokenClaims;
+
+        let userEntity = await UserService.findByEmail(claims.email);
+        if (!userEntity) {
+          userEntity = await UserService.save({
+            password: "ttttttttttttt",
+            oauth2Provider: "google",
+            email: claims.email,
+            name: claims.name,
+          });
+        }
+
+        const token = await jwt.sign({ userId: userEntity.id });
+
+        return redirect(
+          `${process.env.CLIENT_URL}/oauth-success?token=${token}`
+        );
+      } catch (e) {
+        if (e instanceof arctic.OAuth2RequestError) {
+          return status(400, { error: "Invalid authorization code" });
+        }
+        if (e instanceof arctic.ArcticFetchError) {
+          return status(500, { error: "Fetch error" });
+        }
+        return status(500, { error: "Unexpected error" });
       }
-      return status(500, { error: "Unexpected error" });
     }
-  });
+  );

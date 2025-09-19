e3b39089c49e25f41b357402a6bcca51e45589da
iZezito <emersonsilva81240@gmail.com>
2025-09-18 21:36:22 -0300

    railway config deploy

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ const app = new Elysia()
   .use(swagger())
   .use(userController)
   .use(authController)
-  .listen(3000);
+  .listen(process.env.PORT ?? 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
